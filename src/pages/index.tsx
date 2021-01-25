30f663822359d3aab761da60a8156ac1e91f63c6
Diego Masin <diegoifce@gmail.com>
2021-01-25 17:47:15 -0300

    Fazendo a limpa de Exemplos.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,10 @@
-import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
-interface IProduct {
-  id: string;
-  title: string;
-}
-
 export default function Home() {
-  const [recommendedProduct, setRecommendedProduct] = useState<IProduct[]>([]);
-
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_ACESSO}/recommended`).then(response => {
-      response.json().then(data => {
-        setRecommendedProduct(data);
-      });
-    });
-  }, []);
-
   return (
     <div>
       <section>
         <Title>Hello World</Title>
-
-        <ul>
-          {recommendedProduct.map(recommendedProduct => {
-            return <li key="recommendedProduct.id">{recommendedProduct.title}</li>;
-          })}
-        </ul>
       </section>
     </div>
   );
